5ffd077734dd7b1c308a317f1161dc8340ac6b40
coderEdu <educoder78@gmail.com>
2026-03-27 10:14:28 -0300

    Adding random, spell checker and some styles

--- app.js
+++ app.js
@@ -4,10 +4,30 @@ verbDisplay.style.color = "blue";
 const btn_check = document.getElementById("btn_check");
 const verbInput = document.getElementById("verb_input");
 const imgSuccess = document.getElementById("img_success");
+
+
+// Random function
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+// Example: random integer between 1 and 10 (possible results: 1, 2, ..., 9)
+// let randomInt = getRandomInt(1, 10);
+
+var counter = 0;
+var random = getRandomInt(0, Verbs.length);
+var selected_verb = Verbs.at(random).infinitive;
+var spelling = Verbs.at(random).pastSimple;
+
+document.getElementById("verb_display").innerHTML = selected_verb;
+document.getElementById("counter").innerHTML = ++counter + ".";
+
+
 btn_check.addEventListener("click", function() {
     const userInput = verbInput.value.trim().toLowerCase();
     if (userInput !== "") {
-        if (userInput === "went") {
+        if (userInput === spelling) {
             imgSuccess.src = "img/Hopstarter-Sleek-Xp-Basic-Ok.16.png";
         } else {
             imgSuccess.src = "img/Hopstarter-Sleek-Xp-Basic-Close-2.16.png";
---
+++ verbs.js
@@ -0,0 +1,23 @@
+/*
+function Verb(infinitive, pastSimple, pastParticiple, definition, example) {
+  this.infinitive = infinitive;
+  this.pastSimple = pastSimple;
+  this.pastParticiple = pastParticiple;
+  this.definition = definition;
+  this.example = example;
+}
+const go = new Verb("go", "went", "gone", "move from one place to another; travel.", "\"he went out to the shops\"");
+*/
+// -----------------------------------------------------------------------------
+
+const Verbs = [
+  { infinitive: "be", pastSimple: "was/were", pastParticiple: "been", definition: "exist", example: "\"there are no easy answers\"" },
+  { infinitive: "beat", pastSimple: "beat", pastParticiple: "beaten",
+    definition: "strike (a person or an animal) repeatedly and violently so as to hurt or injure them, typically with an implement such as a club or whip.",
+    example: "\"if we were caught we were beaten\"" },
+  { infinitive: "become", pastSimple: "became", pastParticiple: "become", definition: "begin to be.", example: "\"she became angry and sulked all day\"" },
+];
+
+
+
+// { infinitive: "", pastSimple: "", pastParticiple: "", definition: "", example: "" },
