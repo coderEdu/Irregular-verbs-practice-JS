773df0d7034abb742b5ad94160b339c04a059421
coderEdu <educoder78@gmail.com>
2026-04-01 16:31:31 -0300

    The modal windows for the definition and example of the random verb is done!

--- modal.js
+++ modal.js
@@ -1,18 +1,38 @@
-const openModalBtnDef = document.getElementById('open-modal-btn-def');
-const closeModalBtn = document.getElementById('close-modal-btn');
-const myModal = document.getElementById('my-modal');
+// for the definition modal
+const openModalBtnDef = document.getElementById('open_modal_btn_def');
+const closeModalBtnDef = document.getElementById('close_modal_btn_def');
+const myDefModal = document.getElementById('my_def_modal');
 
-const defContent = document.getElementById('def-content'); // Get the element to display the definition
-const defModalTitle = document.getElementById('def-modal-title'); // Get the element for the modal title (optional)
+// for the example modal
+const openModalBtnEg = document.getElementById('open_modal_btn_eg');
+const closeModalBtnEg = document.getElementById('close_modal_btn_eg');
+const myEgModal = document.getElementById('my_eg_modal');
+
+const defContent = document.getElementById('def_content'); // Get the element to display the definition
+const defModalTitle = document.getElementById('def_modal_title'); // Get the element for the modal title (optional)
+const egContent = document.getElementById('eg_content'); // Get the element to display the example
+const egModalTitle = document.getElementById('eg_modal_title'); // Get the element for the example modal title (optional)
 
 // Open the modal
 openModalBtnDef.addEventListener('click', () => {
-  myModal.showModal(); // Displays the dialog as a modal
+  myDefModal.showModal(); // Displays the dialog as a modal
   defContent.textContent = Verbs.at(random).definition; // Set the definition content in the modal
-  defModalTitle.textContent = "Definition: " + Verbs.at(random).infinitive; // Set the modal title to the infinitive form (optional)
+  defModalTitle.textContent = "Definition of: " + Verbs.at(random).infinitive; // Set the modal title to the infinitive form (optional)
 });
 
 // Close the modal
-closeModalBtn.addEventListener('click', () => {
-  myModal.close();
+closeModalBtnDef.addEventListener('click', () => {
+  myDefModal.close();
+});
+
+// Open the example modal
+openModalBtnEg.addEventListener('click', () => {
+    myEgModal.showModal(); // Displays the dialog as a modal
+    egContent.textContent = Verbs.at(random).example; // Set the example content in the modal
+    egModalTitle.textContent = "Example of: " + Verbs.at(random).infinitive; // Set the modal title to the infinitive form (optional)
+});
+
+// Close the example modal
+closeModalBtnEg.addEventListener('click', () => {
+    myEgModal.close();
 });
